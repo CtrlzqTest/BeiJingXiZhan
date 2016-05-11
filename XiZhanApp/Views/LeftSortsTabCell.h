c017f6c22a55ca2d9380fcd9d3da979588004bfc
Ctrlzq <15027164687@163.com>
2016-05-11 10:30:54 +0800

    合并

---
+++ XiZhanApp/Views/LeftSortsTabCell.h
@@ -0,0 +1,13 @@
+//
+//  LeftSortsTabCell.h
+//  XiZhanApp
+//
+//  Created by zhangqiang on 16/5/11.
+//  Copyright © 2016年 zhangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftSortsTabCell : UITableViewCell
+
+@end
---
+++ XiZhanApp/Views/LeftSortsTabCell.m
@@ -0,0 +1,24 @@
+//
+//  LeftSortsTabCell.m
+//  XiZhanApp
+//
+//  Created by zhangqiang on 16/5/11.
+//  Copyright © 2016年 zhangqiang. All rights reserved.
+//
+
+#import "LeftSortsTabCell.h"
+
+@implementation LeftSortsTabCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
