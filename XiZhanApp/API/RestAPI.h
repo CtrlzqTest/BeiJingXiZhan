843370e1a9434bc718c78e7319c74a8e8c9c1550
Ctrlzq <15027164687@163.com>
2016-05-11 14:07:01 +0800

    登录注册功能

--- XiZhanApp/API/RestAPI.h
+++ XiZhanApp/API/RestAPI.h
@@ -10,23 +10,21 @@
 #define _____RestAPI_h
 #import <UIKit/UIKit.h>
 
-//#define BaseAPI                 @"http://192.168.16.139:8080/onepage/"  // 龙龙
+#define BaseAPI                 @"http://192.168.16.139:8080/bjws/"  // 龙龙
 
-#define BaseAPI                 @"http://192.168.16.147:8080/onepage/"  // 公司服务器
+//#define BaseAPI                 @"http://192.168.16.147:8080/onepage/"  // 公司服务器
 
 //#define BaseAPI                 @"http://192.168.16.124:8080/znweb/"    // 赵楠
 
 #define kUpdateDataBaseToHost   @"vote3/test/update.do"
 
-#define kProductListAPI         @"appinfo/plist"
+#define kgetCodeAPI         @"app.user/checkSmsCode"      // 获取验证码
 
 #define kGoodsListAPI           @"appinfo/slist"       // 购物列表
 
-#define kLoginAPI               @"appuser/login"       // 登录
+#define kLoginAPI               @"app.user/login"       // 登录
 
-#define SnsLoginUrlAPI          @"user_login2.action"  // 第三方登录
-
-#define kRegisteAPI             @"appuser/reg"         //注册
+#define kRegisteAPI             @"app.user/register"         //注册
 
 #define kCollectAPI             @"appmyfavorite/save"  //收藏
 
