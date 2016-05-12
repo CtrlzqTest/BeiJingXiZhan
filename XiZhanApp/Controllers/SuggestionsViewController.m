59bf5e014e80b3914397d6816cf18240c5d8d147
zhangqiang <1069507276@qq.com>
2016-05-12 15:15:10 +0800

    geshi

--- XiZhanApp/Controllers/SuggestionsViewController.m
+++ XiZhanApp/Controllers/SuggestionsViewController.m
@@ -13,7 +13,6 @@
 #define textNum 100
 @interface SuggestionsViewController ()<UITextViewDelegate>
 
-@property(nonatomic,retain)UITextField *phoneField;
 @property(nonatomic,retain)UITextView *textView;
 @property(nonatomic,retain)UILabel *countTextLabel;
 @property(nonatomic,retain)UIButton *yesButton;
@@ -44,27 +43,8 @@
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapMethod)];
     [self.view addGestureRecognizer:tap];
     
-    
-    self.phoneField = [[UITextField alloc]init];
-    self.phoneField.frame = CGRectMake(leftSpace, 80*ProportionHeight, KWidth-80*ProportionWidth, 40*ProportionHeight);
-    //self.phoneField.placeholder = @"手机号";
-    self.phoneField.returnKeyType = UIReturnKeyDone;
-    self.phoneField.borderStyle = UITextBorderStyleRoundedRect;
-    self.phoneField.backgroundColor = [UIColor whiteColor];
-   // self.phoneField.enabled = YES;
-    self.phoneField.font = [UIFont systemFontOfSize:17];
-    self.phoneField.layer.cornerRadius = 5.0;
-    self.phoneField.layer.masksToBounds = YES;
-    self.phoneField.layer.borderWidth = 1.0;
-    self.phoneField.layer.borderColor = colorref;
-    self.phoneField.autocapitalizationType = NO;
-    self.phoneField.text = [Utility getUserInfoFromLocal][@"tel"];
-    self.phoneField.clearButtonMode = UITextFieldViewModeWhileEditing;
-    
-  //  [self.view addSubview:self.phoneField];
-    
     self.textView = [[UITextView alloc]init];
-    self.textView.frame = CGRectMake(leftSpace, CGRectGetMaxY(self.phoneField.frame) + lineSpace, KWidth-80, 120);
+    self.textView.frame = CGRectMake(leftSpace, 130*ProportionHeight, KWidth-80, 120);
     self.textView.backgroundColor = [UIColor whiteColor];
     self.textView.editable = YES;
     self.textView.font = [UIFont systemFontOfSize:14];
@@ -186,19 +166,6 @@
 
 - (BOOL )checkInput {
     
-    NSLog(@"%@",[Utility getUserInfoFromLocal][@"tel"]);
-    if (self.phoneField.text.length <= 0) {
-        [MBProgressHUD showError:@"手机号不能为空" toView:nil];
-        return NO;
-    }
-    if (![Utility checkTelNumber:self.phoneField.text]) {
-        [MBProgressHUD showError:@"手机号格式不正确" toView:nil];
-        return NO;
-    }
-//    if ( [User shareUser].isLogin) {
-//        self.phoneField.text = [Utility getUserInfoFromLocal][@"tel"];
-//        self.phoneField.enabled = NO;
-//    }
     if (self.textView.text.length <= 0) {
         [MBProgressHUD showError:@"意见不能为空" toView:nil];
         return NO;
