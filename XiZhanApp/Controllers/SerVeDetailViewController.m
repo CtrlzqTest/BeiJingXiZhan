9b87926f09540d50bfc952173460232c2608636e
zhangqiang <1069507276@qq.com>
2016-05-15 13:46:19 +0800

    服务台详情

--- XiZhanApp/Controllers/SerVeDetailViewController.m
+++ XiZhanApp/Controllers/SerVeDetailViewController.m
@@ -8,6 +8,8 @@
 
 #import "SerVeDetailViewController.h"
 #import "MessageModel.h"
+#import "UIImageView+AFNetworking.h"
+#import "UIViewController+AYCNavigationItem.h"
 
 @interface SerVeDetailViewController ()<UIWebViewDelegate>
 @property(nonatomic,copy)NSString *stringOftext;
@@ -41,15 +43,31 @@
 }
 -(void)initView
 {
+    self.view.backgroundColor = [UIColor whiteColor];
+        [self setTextTitleViewWithFrame:CGRectMake(180*ProportionWidth, 0, 120*ProportionWidth, 40*ProportionWidth) title:@"详情" fontSize:17.0];
+    
     self.stringOftext = self.model.msgtitle;
     self.stringOftext = [self.stringOftext stringByAppendingString:@"\n"];
     self.stringOftext = [self.stringOftext stringByAppendingString:self.model.msgcontent];
     
-    UIWebView *web = [[UIWebView alloc]init];
-    web.delegate = self;
-    web.frame = CGRectMake(0, 0, KWidth, KHeight);
+    UILabel *label = [[UILabel alloc]init];
+    label.frame = CGRectMake(0, 0, KWidth, KHeight*1.0/3);
+    label.text = [NSString stringWithFormat:@"%@",self.stringOftext];
+    [self.view addSubview:label];
     
-    [self.view addSubview:web];
-    [web loadHTMLString:self.stringOftext baseURL:nil];
+    NSArray *array = [self.model.imgurl componentsSeparatedByString:@","];
+    if (array.count == 0) {
+        
+    }
+    else
+    {
+        for (int i = 0;i < array.count ;i++) {
+            UIImageView *imgv = [[UIImageView alloc]init];
+            imgv.frame = CGRectMake(i*1.0/3*KWidth, 1.0/3*KHeight, 1.0/3*KWidth-20*ProportionWidth, 1.0/3*KHeight);
+            [imgv setImageWithURL:[NSURL URLWithString:array[i]]];
+            [self.view addSubview:imgv];
+        }
+    }
+
 }
 @end
