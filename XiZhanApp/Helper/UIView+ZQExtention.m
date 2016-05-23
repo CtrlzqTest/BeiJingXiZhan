8eaab3041a5267b7a0d52a8153e1e9ce34bbac0f
张强 <apple@apples-mac-mini.local>
2016-05-23 16:22:30 +0800

    修改BUG

--- XiZhanApp/Helper/UIView+ZQExtention.m
+++ XiZhanApp/Helper/UIView+ZQExtention.m
@@ -24,12 +24,13 @@
     CGFloat height = self.bounds.size.height;
     
     CAGradientLayer *gradientLayer = [CAGradientLayer layer];
-    gradientLayer.frame = CGRectMake(0, -20, width, height + 20);
+    gradientLayer.frame = CGRectMake(0, 0, width, height + 20);
     [gradientLayer setColors:[NSArray arrayWithObjects:(id)(startColor.CGColor),(id)(endColor.CGColor), nil]];
     // 渐变方式,一般X相同,渐变方式为上到下(或下到上),Y相同的话就是左到右(或右到左)
     gradientLayer.startPoint = CGPointMake(0, 0.5);
     gradientLayer.endPoint = CGPointMake(1, 0.5);
-    [self.layer insertSublayer:gradientLayer atIndex:1];
+    CALayer *layer = self.layer.sublayers[0];
+    [layer addSublayer:gradientLayer];
     
 }
 
