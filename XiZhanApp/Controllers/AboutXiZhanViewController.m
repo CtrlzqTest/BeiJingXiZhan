0ae0e8175778125765f0db7877104c8c2068030e
zhangqiang <1069507276@qq.com>
2016-05-26 14:31:13 +0800

    关于我们，免责申明

--- XiZhanApp/Controllers/AboutXiZhanViewController.m
+++ XiZhanApp/Controllers/AboutXiZhanViewController.m
@@ -1,20 +1,18 @@
 //
-//  MianZeViewController.m
+//  AboutXiZhanViewController.m
 //  XiZhanApp
 //
-//  Created by zhangqiang on 16/5/10.
+//  Created by zhangqiang on 16/5/26.
 //  Copyright © 2016年 zhangqiang. All rights reserved.
 //
 
-#import "MianZeViewController.h"
+#import "AboutXiZhanViewController.h"
 
-#import "UIViewController+AYCNavigationItem.h"
-
-@interface MianZeViewController ()
+@interface AboutXiZhanViewController ()
 @property(nonatomic,retain)UIImageView *imgV;
 @end
 
-@implementation MianZeViewController
+@implementation AboutXiZhanViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -26,7 +24,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 -(void)initView
 {
     // 返回按钮
@@ -34,8 +31,8 @@
     [self setLeftImageBarButtonItemWithFrame:CGRectMake(0, 0, 30, 30) image:@"back" selectImage:nil action:^(AYCButton *button) {
         [weakSelf.navigationController popViewControllerAnimated:YES];
     }];
-    [self setTextTitleViewWithFrame:CGRectMake(180*ProportionWidth, 0, 120*ProportionWidth, 50*ProportionWidth)title:@"免责申明条款" fontSize:17.0];
-   // self.view.backgroundColor = [UIColor colorWithRed:0.773 green:0.153 blue:0.384 alpha:1.000];
+    [self setTextTitleViewWithFrame:CGRectMake(180*ProportionWidth, 0, 120*ProportionWidth, 50*ProportionWidth)title:@"关于我们" fontSize:17.0];
+    // self.view.backgroundColor = [UIColor colorWithRed:0.773 green:0.153 blue:0.384 alpha:1.000];
     self.view.backgroundColor = [UIColor blueColor];
     _imgV = [[UIImageView alloc]initWithFrame:CGRectMake(0, 60*ProportionHeight, KWidth, KHeight-60*ProportionHeight)];
     _imgV.image = [UIImage imageNamed:@"about_info.png"];
