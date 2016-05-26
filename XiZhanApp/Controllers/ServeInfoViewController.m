1c8bbda892c06541e520394ac78e70583bce4eb4
zhangqiang <1069507276@qq.com>
2016-05-26 12:43:50 +0800

    tibia

--- XiZhanApp/Controllers/ServeInfoViewController.m
+++ XiZhanApp/Controllers/ServeInfoViewController.m
@@ -43,8 +43,11 @@ static NSString *serveCellId = @"serveTabCellId";
        // [self requestData];
          _shouldRefresh = YES;
         [self.navigationController.navigationBar setGradientLayerStartColor:[UIColor colorWithRed:0.110 green:0.690 blue:0.859 alpha:1.000] endColor:[UIColor colorWithRed:0.067 green:0.388 blue:0.635 alpha:1.000]];
-        self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:self action:@selector(backMethod)];
-        self.navigationItem.leftBarButtonItem.tintColor = [UIColor whiteColor];
+        __weak typeof(self) weakSelf = self;
+        [self setLeftImageBarButtonItemWithFrame:CGRectMake(0, 0, 30, 30) image:@"back" selectImage:nil action:^(AYCButton *button) {
+            [weakSelf backMethod];
+        }];
+
     }
     
   
