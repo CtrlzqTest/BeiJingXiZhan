cbee3f534b9d2f36c25478b950d3431d558f9379
张强 <apple@apples-mac-mini.local>
2016-05-30 18:26:30 +0800

    push

--- XiZhanApp/API/NetwrokHandle/MHAsiNetworkItem.m
+++ XiZhanApp/API/NetwrokHandle/MHAsiNetworkItem.m
@@ -62,8 +62,8 @@
         DTLog(@"----请求参数%@\n",params);
         AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
         manager.responseSerializer = [AFJSONResponseSerializer serializer];
-        manager.responseSerializer.acceptableContentTypes =  [NSSet setWithObject:@"text/html"];
-//        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/xml", nil];
+//        manager.responseSerializer.acceptableContentTypes =  [NSSet setWithObject:@"text/html"];
+        manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/xml",@"text/html", nil];
         //设置请求超时时长
         [manager.requestSerializer setTimeoutInterval:10];
 //        manager.requestSerializer = [AFJSONRequestSerializer serializer];
