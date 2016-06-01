2c187e24703fc4648ffa36884486788ea01e625c
zhangqiang <1069507276@qq.com>
2016-06-01 11:20:31 +0800

    提交发布

--- XiZhanApp/Controllers/ServeInfoViewController.m
+++ XiZhanApp/Controllers/ServeInfoViewController.m
@@ -111,7 +111,7 @@ static NSString *serveCellId = @"serveTabCellId";
     }else {
       //  PublishInfoViewController *publishVC = [Utility getControllerWithStoryBoardId:ZQPublishInfoViewControllerId];
        PublishViewController *publishVC = [[PublishViewController alloc]init];
-        publishVC.parentIdString = self.parentIdString;
+        publishVC.parentIdString = self.menuModel.menuId;
         publishVC.menuModel = self.menuModel;
         publishVC.delegate = self;
         [self.navigationController pushViewController:publishVC animated:YES];
