dd4460d181225fa323052e23bfaa93341506bb03
Ctrlzq <15027164687@163.com>
2016-06-02 15:36:28 +0800

    commit

--- XiZhanApp/Controllers/TaxiClassifyViewController.m
+++ XiZhanApp/Controllers/TaxiClassifyViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "TaxiClassifyViewController.h"
+#import "MenuType2TabCell.h"
+#import <MJRefresh.h>
 
-@interface TaxiClassifyViewController ()
+@interface TaxiClassifyViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    NSInteger _page;
+    BOOL _shouldRefresh; // 是否需要刷新数据
+    NSMutableArray *_dataArray;
+}
+@property(nonatomic,strong)UITableView *tableView;
 
 @end
 
@@ -17,6 +25,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)setupViews {
+    
+    _dataArray = [NSMutableArray array];
+    // 返回按钮
+    __weak typeof(self) weakSelf = self;
+    [self setLeftImageBarButtonItemWithFrame:CGRectMake(0, 0, 30, 30) image:@"back" selectImage:nil action:^(AYCButton *button) {
+        [weakSelf.navigationController popViewControllerAnimated:YES];
+    }];
+    
+    self.tableView = [[UITableView alloc]init];
+    self.tableView.frame = CGRectMake(0, 0, KWidth, KHeight);
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    // 注册cell
+    [self.tableView registerNib:[UINib nibWithNibName:@"MenuType2TabCell" bundle:nil] forCellReuseIdentifier:cellId];
+    
+    self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [self getData];
+    }];
+    [self.tableView.mj_header beginRefreshing];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+}
+
+- (void)getData {
+    
+    
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _dataArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MenuType2TabCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    MenuModel *model = _dataArray[indexPath.row];
+    [cell writeDataWithModel:model];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MenuModel *model = _dataArray[indexPath.row];
+//    MyInformationsViewController *myInfoVC = [Utility getControllerWithStoryBoardId:@"myInfoVC"];;
+//    myInfoVC.menuModel = model;
+//    [self.navigationController pushViewController:myInfoVC animated:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
