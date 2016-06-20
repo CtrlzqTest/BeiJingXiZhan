dfef084ca2188e56e067693e91f858c55fc37a40
zhangqiang <1069507276@qq.com>
2016-06-20 10:00:23 +0800

    推送角标

--- XiZhanApp/AppDelegate.m
+++ XiZhanApp/AppDelegate.m
@@ -125,10 +125,10 @@
 {
     NSInteger count = --[UIApplication sharedApplication].applicationIconBadgeNumber;
     if (count >= 0) {
+        _bageIsZero = YES;
         [JPUSHService clearAllLocalNotifications];
         [JPUSHService setBadge:count];
     }
-    _bageIsZero = YES;
     [JPUSHService handleRemoteNotification:userInfo];
 
 //    NSDictionary *dict = userInfo[@"aps"];
@@ -186,7 +186,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    if (!_bageIsZero)
+    if (_bageIsZero)
     {
         [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
         [JPUSHService clearAllLocalNotifications];
